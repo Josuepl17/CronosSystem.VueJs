909ed7f6f24156c2e1bed8a3d568fd25c832a1ab
Josuepl17 <136339183+Josuepl17@users.noreply.github.com>
2024-10-04 22:38:45 -0300

    Migration Users Pronta

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->integer('empresa_id'); // so para Seleção de filiais 
             $table->timestamps();
         });
 
