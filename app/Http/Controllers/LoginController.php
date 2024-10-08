3d1aa447fffca11aa30ee62b236d010995dc2ccb
Josuepl17 <136339183+Josuepl17@users.noreply.github.com>
2024-10-08 12:44:47 -0300

    Update LoginController.php

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Empresas;
+use App\Models\User;
 use App\Models\User_Empresas;
 use App\Models\Users;
 use App\Services\MeuServico;
 use GuzzleHttp\Psr7\Request as Psr7Request;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Inertia\Inertia;
 
 class LoginController extends Controller
@@ -26,11 +28,11 @@ class LoginController extends Controller
 
 
     public function criar_empresa_user(Request $request) {
-        $user = new Users();
+        $user = new User();
         $user->name = $request->name;
         $user->second_name = $request->second_name;
         $user->email = $request->email;
-        $user->password = $request->password;
+        $user->password = Hash::make($request->password);
         $user->save();
 
         $empresa = new Empresas();
@@ -50,9 +52,13 @@ class LoginController extends Controller
 
     public function autenticate(Request $request) {
         $credentials = $request->only('email', 'password');
-        
+      
         if (Auth::attempt(['email' => $credentials['email'], 'password' => $credentials['password']])) {
-            return Inertia::render('index');    }
+           // return Inertia::render('index');  
+           dd("deu bom");
+          } else {
+            dd("deu ruim");
+          }
 
 }
 };
