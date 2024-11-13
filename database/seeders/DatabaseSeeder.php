80e3b82fd70d5e6212f3c62638b2abc24695cc25
Josuepl17 <136339183+Josuepl17@users.noreply.github.com>
2024-11-13 19:30:09 -0300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,13 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(UserSeeder::class);
-        
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
     }
 
 
