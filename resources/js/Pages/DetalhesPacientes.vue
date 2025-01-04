42f35057363cbcd4b371379aede9d9349b4ff370
Josué Lima <136339183+Josuepl17@users.noreply.github.com>
2025-01-04 10:38:05 -0300

    top

--- resources/js/Pages/DetalhesPacientes.vue
+++ resources/js/Pages/DetalhesPacientes.vue
@@ -207,6 +207,7 @@ const fechararquivos = () => {
 
 <style scoped>
 @import "../Components/css/tabelas.css";
+@import "../Components/css/modal.css";
 
 .notification {
   position: fixed;
@@ -304,37 +305,7 @@ const fechararquivos = () => {
 }
 </style>
 
-<style>
-.modal-overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.7);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 1000;
-}
 
-.modal-content {
-  background-color: white;
-  padding: 20px;
-  border-radius: 8px;
-  width: 850px;
-  height: 480px;
-  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-  z-index: 1001;
-  transition: ease-in-out 1s;
-  text-align: center;
-}
-
-img {
-  width: 20px;
-  height: 20px;
-}
-</style>
 
 <style scoped>
 .form-group {
